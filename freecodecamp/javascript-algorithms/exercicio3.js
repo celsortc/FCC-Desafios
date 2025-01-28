65be1378aa336884b11099c221cd594249018787
Celso <celsortc@outlook.com>
2025-01-27 22:16:58 -0300

    array ao contrario

--- freecodecamp/javascript-algorithms/exercicio3.js
+++ freecodecamp/javascript-algorithms/exercicio3.js
@@ -1,5 +1,9 @@
 let i = 0;
 
-for (i = 0; i < 10; i++) {
-  console.log(i);
+let teste = ["Simba", "Nadal", "Bia", "Marmaduke"];
+
+for (i = teste.length - 1; i >= 0; i--) {
+  console.log(teste[i]);
 }
+
+// array ao contrario
